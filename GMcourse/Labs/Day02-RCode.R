51063b810e8d821a9b5195962289073b7357f26a
Antigoni Kaliontzopoulou <antkal@gmail.com>
2026-06-01 19:39:07 -0500

    Updates to script day 2 and others

--- GMcourse/Labs/Day02-RCode.R
+++ GMcourse/Labs/Day02-RCode.R
@@ -3,7 +3,67 @@
 
 library(geomorph)
 
-##### 1: ANOVA and pairwise comparisons ==============================================================
+##### 1: Visualizing Shape Differences and PCA ####
+
+# Plotting all specimens
+data(plethodon)
+Y.gpa <- gpagen(plethodon$land, print.progress = F)    # GPA-alignment
+
+par(mfrow=c(1,2)) 
+plotAllSpecimens(plethodon$land, links=plethodon$links)  # Raw data
+mtext("Raw Data")
+plotAllSpecimens(Y.gpa$coords, links=plethodon$links)    # GPA-aligned data
+mtext("GPA-Aligned Specimens")
+par(mfrow=c(1,1)) 
+
+# Types of deformations
+ref <- mshape(Y.gpa$coords)
+par(mfrow=c(2,2))
+plotRefToTarget(ref,Y.gpa$coords[,,39], links=plethodon$links)
+mtext("TPS")
+plotRefToTarget(ref,Y.gpa$coords[,,39],mag=2.5, links=plethodon$links)
+mtext("TPS: 2.5X magnification")
+
+plotRefToTarget(ref,Y.gpa$coords[,,39], links=plethodon$links,method="vector",mag=3)
+mtext("Vector Displacements")
+plotRefToTarget(ref,Y.gpa$coords[,,39], links=plethodon$links,gridPars=gridPar(pt.bg="red", link.col="green", pt.size = 1),
+                method="vector",mag=3)
+mtext("Vector Displacements: Other Options")
+dev.off()
+
+# Principal Components Analysis (PCA)
+pleth.raw <- gm.prcomp(Y.gpa$coords)
+gps <- as.factor(paste(plethodon$species, plethodon$site))
+plot(pleth.raw)
+par(mar=c(2, 2, 2, 2))
+plot(pleth.raw, pch=22, cex = 1.5, bg = gps) 
+#  Add things as desired using standard R plotting
+legend("topleft", pch=22, pt.bg = unique(gps), legend = levels(gps))
+
+# Shape Predictions
+# PCA-based
+M <- mshape(Y.gpa$coords)
+PC <- pleth.raw$x[,1]
+preds <- shape.predictor(Y.gpa$coords, x= PC, Intercept = FALSE, 
+                         pred1 = min(PC), pred2 = max(PC)) # PC 1 extremes, more technically
+plotRefToTarget(M, preds$pred1, links = plethodon$links)
+mtext("PC1 - Min.")
+plotRefToTarget(M, preds$pred2, links = plethodon$links)
+mtext("PC1 - Max.")
+
+# Note: gm.prcomp already provides these shapes!
+names(pleth.raw)
+names(pleth.raw$shapes)
+str(pleth.raw$shapes$shapes.comp1)
+
+##### PickNPlot Shapes in Real Time (more detail here)
+pleth.pca.plot <- plot(pleth.raw)
+picknplot.shape(pleth.pca.plot) 
+
+picknplot.shape(plot(pleth.pca), method = "points", mag = 3, links=plethodon$links)
+
+
+##### 2: ANOVA and pairwise comparisons ==============================================================
 
 data(plethodon) # SOME EXAMPLE DATA
 Y.gpa <- gpagen(plethodon$land, print.progress = FALSE)  
@@ -123,7 +183,7 @@ fit <- procD.lm(coords ~ treatment/family, data = gdf,
 anova(fit) # treatment effect not adjusted
 anova(fit, error = c("treatment:family", "Residuals")) # treatment effect updated (adjusted)
 
-##### 2: Allometry ===================================================================================
+##### 3: Allometry ===================================================================================
 
 # Simple Allometry
 data(plethodon) 
@@ -194,109 +254,3 @@ picknplot.shape(pc.plot)
 dev.off()
 ### --------------------------------------------------------------------------------------------------
 
-##### 3: Visualizing Shape Differences and PCA ####
-
-# Plotting all specimens
-data(plethodon)
-Y.gpa <- gpagen(plethodon$land, print.progress = F)    # GPA-alignment
-
-par(mfrow=c(1,2)) 
-plotAllSpecimens(plethodon$land, links=plethodon$links)  # Raw data
-mtext("Raw Data")
-plotAllSpecimens(Y.gpa$coords, links=plethodon$links)    # GPA-aligned data
-mtext("GPA-Aligned Specimens")
-par(mfrow=c(1,1)) 
-
-# Types of deformations
-ref <- mshape(Y.gpa$coords)
-par(mfrow=c(3,2))
-plotRefToTarget(ref,Y.gpa$coords[,,39], links=plethodon$links)
-mtext("TPS")
-plotRefToTarget(ref,Y.gpa$coords[,,39],mag=2.5, links=plethodon$links)
-mtext("TPS: 2.5X magnification")
-
-plotRefToTarget(ref,Y.gpa$coords[,,39], links=plethodon$links,method="vector",mag=3)
-mtext("Vector Displacements")
-plotRefToTarget(ref,Y.gpa$coords[,,39], links=plethodon$links,gridPars=gridPar(pt.bg="red", link.col="green", pt.size = 1),
-                method="vector",mag=3)
-mtext("Vector Displacements: Other Options")
-
-plotRefToTarget(ref,Y.gpa$coords[,,39],mag=2,outline=plethodon$outline)  
-mtext("Outline Deformation")
-plotRefToTarget(ref,Y.gpa$coords[,,39],method="points",outline=plethodon$outline)
-mtext("OUtline Deformations Ref (gray) & and Tar (black)")
-par(mfrow=c(1,1))
-
-# Shape Predictions
-# PCA-based
-M <- mshape(Y.gpa$coords)
-PCA <- gm.prcomp(Y.gpa$coords)
-PC <- PCA$x[,1]
-preds <- shape.predictor(Y.gpa$coords, x= PC, Intercept = FALSE, 
-                         pred1 = min(PC), pred2 = max(PC)) # PC 1 extremes, more technically
-plotRefToTarget(M, preds$pred1, links = plethodon$links)
-mtext("PC1 - Min.")
-plotRefToTarget(M, preds$pred2, links = plethodon$links)
-mtext("PC1 - Max.")
-
-# Regression-based
-gdf <- geomorph.data.frame(Y.gpa)
-plethAllometry <- procD.lm(coords ~ log(Csize), data=gdf, print.progress = FALSE)
-allom.plot <- plot(plethAllometry, 
-                   type = "regression", 
-                   predictor = log(gdf$Csize),
-                   reg.type ="PredLine") # make sure to have a predictor 
-
-preds <- shape.predictor(plethAllometry$GM$fitted, x= allom.plot$RegScore, Intercept = FALSE, 
-                         predmin = min(allom.plot$RegScore), 
-                         predmax = max(allom.plot$RegScore)) 
-plotRefToTarget(M, preds$predmin, mag=3, links = plethodon$links)
-plotRefToTarget(M, preds$predmax, mag=3, links = plethodon$links)
-
-# via picknplot.shape (more detail below)
-picknplot.shape(allom.plot) 
-
-# Group difference-based
-gdf <- geomorph.data.frame(Y.gpa, species = plethodon$species, site = plethodon$site)
-pleth.anova <- procD.lm(coords ~ species*site, data=gdf, print.progress = FALSE)
-X <- pleth.anova$X
-X # includes intercept; remove for better functioning 
-X <- X[,-1]
-symJord <- c(0,1,0) # design for P. Jordani in sympatry
-alloJord <- c(0,0,0) # design for P. Jordani in allopatry
-preds <- shape.predictor(pleth.anova$GM$fitted, x = X, Intercept = TRUE, 
-                         symJord=symJord, alloJord=alloJord)
-plotRefToTarget(M, preds$symJord, links = plethodon$links, mag=2)
-plotRefToTarget(M, preds$alloJord, links = plethodon$links, mag=2)
-
-# via picknplot.shape (more detail below)
-plot.anova <- plot(pleth.anova, type = "PC", pch = 21, 
-                   bg = interaction(gdf$species, gdf$site), 
-                   asp = 1)
-
-picknplot.shape(plot.anova) 
-
-##### 3: Principal Components Analysis (PCA)
-pleth.raw <- gm.prcomp(Y.gpa$coords)
-gps <- as.factor(paste(plethodon$species, plethodon$site))
-plot(pleth.raw)
-par(mar=c(2, 2, 2, 2))
-plot(pleth.raw, pch=22, cex = 1.5, bg = gps) 
-#  Add things as desired using standard R plotting
-legend("topleft", pch=22, pt.bg = unique(gps), legend = levels(gps))
-
-##### 4: PickNPlot Shapes in Real Time (more detail here)
-data(plethodon) 
-Y.gpa <- gpagen(plethodon$land)
-pleth.pca <- gm.prcomp(Y.gpa$coords)
-
-pleth.pca.plot <- plot(pleth.pca)
-picknplot.shape(pleth.pca.plot) 
-
-picknplot.shape(plot(pleth.pca), method = "points", mag = 3, links=plethodon$links)
-
-##### 5: 3D Warping
-scallops <- readland.tps("Data/scallops for viz.tps", specID = "ID")
-ref <- mshape(scallops)
-refmesh <- warpRefMesh(read.ply("Data/glyp02L.ply"), 
-                       scallops[,,1], ref, color=NULL, centered=T)
